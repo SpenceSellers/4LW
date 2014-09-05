0fb59fd4ee4974f5cb22f83c1007aa303ed1340d
Spencer Sellers <spencesellers@gmail.com>
2014-09-05 08:27:53 -0500

    Calculate instruction length.

--- Instruction.hs
+++ Instruction.hs
@@ -21,6 +21,7 @@ data InstructionParseResult = InstructionParseResult Instruction Int
 
 parseInstruction :: Word -> Memory.Memory -> Either BadInstruction InstructionParseResult
 parseInstruction addr mem = undefined
-    where lengthOffset = toWord 2
+    where lengthOffset = 2
           opcode = (Memory.readLetter mem addr, Memory.readLetter mem (offset addr 1))
+          instructionLength = getValue $ Memory.readLetter mem (offset addr lengthOffset)
           
