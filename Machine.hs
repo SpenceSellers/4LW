87ba890dd241fe4591a875edba3e64b8e6cc49e2
Spencer Sellers <spencesellers@gmail.com>
2015-02-17 18:42:06 -0600

    Getting and setting data is now done using the State monad, as it should have been.

--- Machine.hs
+++ Machine.hs
@@ -53,43 +53,46 @@ setPC :: Word -> State MachineState ()
 setPC addr = registers.(ix pcRegister) .= addr
 
 -- | Fetches data from a DataLocation.
-getData :: MachineState -> DataLocation -> Word
-getData state (Constant word) = word
-getData state (Register letter) =
-    if inRange registerBounds letter
-    then (state^.registers) ! letter
-    else minWord
-getData state (MemoryLocation addr) = either (const minWord) (id) $ Memory.readWord (state^.memory) addr
+getData :: DataLocation -> State MachineState Word
+getData (Constant word) = return word
+getData (Register letter) = do
+  state <- get
+  return $ if inRange registerBounds letter
+           then (state^.registers) ! letter
+           else minWord
+                
+getData (MemoryLocation addr) = do
+  state <- get
+  return $ either (const minWord) (id) $ Memory.readWord (state^.memory) addr
 
 -- | Applies a data write to any location, be it a register, main memory, etc.
-setData :: MachineState -> DataLocation -> Word -> MachineState
-setData state (Constant const) word = state -- No-op for now. Raise interrupt later.
-                                      
-setData state (Register letter) word =
-    over registers (\regs -> regs  // [(letter, word)]) $ state
-         
-setData state (MemoryLocation addr) word =
-    set memory (Memory.writeWord (state^.memory) addr word) $ state
+setData :: DataLocation -> Word -> State MachineState ()
+setData (Constant const) word = return () -- No-op for now. Raise interrupt later.
+setData (Register letter) word =
+    registers %= (\regs -> regs  // [(letter, word)])
+      
+setData (MemoryLocation addr) word =
+  memory %= \mem -> (Memory.writeWord mem addr word)
 
 -- | Applies an instruction to the state of the Machine.
 runInstruction :: Instruction -> State MachineState ()
 runInstruction (Move src dest) = do
   state <- get
-  let datum = getData state src
-  put $ setData state dest datum
+  datum <- getData src
+  setData dest datum
 
 runInstruction (Add src1 src2 dest) = do
   state <- get
-  let data1 = getData state src1
-      data2 = getData state src2
-  put $ setData state dest (addWord data1 data2)
+  data1 <- getData src1
+  data2 <- getData src2
+  setData dest (addWord data1 data2)
       
 tick :: State MachineState ()
 tick = do
   runInstruction (Move (Constant maxWord) (Register (Letter 'B')))
   runInstruction (Move (Constant maxWord)
                   (MemoryLocation
-                   (Word (letter 'A', letter 'A', letter 'A', letter 'A'))))
+                   (Word (letter '_', letter '_', letter 'A', letter 'A'))))
   return ()
 
   
